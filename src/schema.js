37be89097d5beb8cb3e7481fe6a974150b6dcef8
Ajaypatel932000 <ap210484@gmail.com>
2022-08-24 12:47:03 +0530

    restage

--- src/schema.js
+++ src/schema.js
@@ -35,8 +35,9 @@ const typeDefs = gql`
     author: Author!
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
-    "The track's approximate length to complete, in minutes"
+    "The track's approximate length to complete, in seconds"
     length: Int
+
     "The number of modules this track contains"
     modulesCount: Int
     "The track's complete description, can be in markdown format"
@@ -61,13 +62,22 @@ const typeDefs = gql`
     id: ID!
     "The module's title"
     title: String!
-    "The module's length in minutes"
+
+    "The module's length in seconds"
     length: Int
+
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
     videoUrl: String
   }
 `;
+// @deprecated(reason: "Use durationInSeconds")
 
+// "The track's full duration, in seconds"
+// durationInSeconds: Int
+
+// @deprecated(reason: "Use durationInSeconds")
+// "The track's full duration, in seconds"
+// durationInSeconds: Int
 module.exports = typeDefs;
